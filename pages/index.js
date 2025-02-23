dc2b6099acb0b230064e2f8af470c94d18be7171
محمد عسکری <0520352319@tci.ir>
2025-02-23 16:04:34 +0330

    feat:about section in index page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Swiper from 'swiper'
 import Slider from './components/templates/Index/Slider'
+import About from './components/templates/Index/About'
 
 const Home = () => {
   return (
     <>
     <Slider/>
+    <About/>
     </>
   )
 }
