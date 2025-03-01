7d88088b72042d3a978997042ea029616aa243e6
محمد عسکری <m.asgari91@gmail.com>
2025-03-01 13:56:20 +0330

    feat:Testimonial part in index page

---
+++ pages/components/templates/Index/Testimonials.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Testimonial from '../../modules/Testimonial/Testimonial'
+
+const Testimonials = ({comments}) => {
+  return (
+    <div className="container-fluid py-5">
+        <div className="container">
+            <div className="section-title">
+                <h4 className="text-primary text-uppercase" style={{letterSpacing: "5px"}}>Testimonial</h4>
+                <h1 className="display-4">Our Clients Say</h1>
+            </div>
+            <div className="owl-carousel testimonial-carousel">
+              {comments.slice(0,4).map(comment=>(
+
+              <Testimonial comment={comment} key={comment.id}/>
+              ))}
+              
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Testimonials
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Services from './components/templates/Index/Services'
 import Offer from './components/templates/Index/Offer'
 import Menu from './components/templates/Index/Menu'
 import Reservation from './components/templates/Index/Reservation'
+import Testimonials from './components/templates/Index/Testimonials'
 
 const Home = ({data}) => {
   return (
@@ -16,6 +17,7 @@ const Home = ({data}) => {
     <Offer/>
     <Menu menu={data.menu}/>
     <Reservation/>
+    <Testimonials comments={data.comments}/>
     </>
   )
 }
@@ -24,14 +26,17 @@ export async function getStaticProps() {
   const servicesData=await servicesRes.json()
   const menuRes=await fetch("http://localhost:4000/menu")
   const menuData=await menuRes.json()
+  const commentsRes=await fetch("http://localhost:4000/comment")
+  const commentData=await commentsRes.json()
   return {
     props:{
       data:{
         services:servicesData,
-        menu:menuData
+        menu:menuData,
+        comments:commentData
       }
     },
-    revalidate:60*60*12
+    revalidate:60*60*12 //second
   
   }
 }
