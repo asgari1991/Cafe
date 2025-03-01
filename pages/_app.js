b7cd6eef840067e9d8c4b615a73767dbb5f5ce5c
محمد عسکری <m.asgari91@gmail.com>
2025-03-01 15:04:59 +0330

    implementing Footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import NavBar from "./components/modules/NavBar/NavBar";
+import Footer from "./components/modules/Footer/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
     <NavBar/>
     <Component {...pageProps} />
+    <Footer/>
     </>
   );
 }
